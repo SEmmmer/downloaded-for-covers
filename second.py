655bf1f0843c9fec9844d54a0bd6384ce4dc4807
Shirakami Emmmer <yjhdandan@163.com>
2020-02-21 16:02:13 +0800

    add regex to url match for img

--- second.py
+++ second.py
@@ -1,14 +1,18 @@
 from __future__ import print_function
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+import re
 
 son_html = urlopen('https://www.youtube.com/watch?v=cXP8OxMN_SQ').read().decode('utf-8')
 son_soup = BeautifulSoup(son_html, 'lxml')
 
 for title in son_soup.find_all('h1'):
     print(title)
-for date in son_soup.find_all('yt-formatted-string'):
-    print(date)
+
+m = re.search('(?<=v=)...........', 'https://www.youtube.com/watch?v=cXP8OxMN_SQ')
+m.group(0)
+print(m.group(0))
+
 # ok_all_info = son_soup
 # for i in ok_all_info:
 #         if (len(i))<100:
